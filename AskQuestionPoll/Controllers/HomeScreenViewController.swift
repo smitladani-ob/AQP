e8469697dd1d830861dadc625541378f110dbf5c
smitladani-ob <smitladani.ob@gmail.com>
2026-03-21 15:13:21 +0530

    outlet connects

--- AskQuestionPoll/Controllers/HomeScreenViewController.swift
+++ AskQuestionPoll/Controllers/HomeScreenViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class HomeScreenViewController: UIViewController {
 
+    //first container
     @IBOutlet weak var underLineTextView: UIView!
     @IBOutlet weak var charCounter: UILabel!
     @IBOutlet weak var descriptionTextView: UITextView!
@@ -17,6 +18,30 @@ class HomeScreenViewController: UIViewController {
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var selectorImage: UIImageView!
     @IBOutlet weak var selectCategoryField: SelectCatagoryView!
+    
+    //second Container
+    @IBOutlet weak var optionsView: RadioButtonsView!
+    @IBOutlet weak var optionOneImageBg: UIImageView!
+    @IBOutlet weak var optionOneTextview: UITextView!
+    @IBOutlet weak var optionOneView: UIView!
+    @IBOutlet weak var optionOneLOne: UILabel!
+    @IBOutlet weak var optionOneLTwo: UILabel!
+    @IBOutlet weak var optionTwoImageBg: UIImageView!
+    @IBOutlet weak var optionTwoTextview: UITextView!
+    @IBOutlet weak var optionTwoView: UIView!
+    @IBOutlet weak var optionTwoLOne: UILabel!
+    @IBOutlet weak var optionTwoLTwo: UILabel!
+    
+    //Third Container
+    @IBOutlet weak var prefernceLabel: UILabel!
+    @IBOutlet weak var chooseLocationField: SelectCatagoryView!
+    @IBOutlet weak var afterLocationLbl: UILabel!
+    @IBOutlet weak var chooseGenderField: SelectCatagoryView!
+    @IBOutlet weak var afterGenderLbl: UILabel!
+    
+    //Fourth Container
+    @IBOutlet weak var submitButton: yellowButtonView!
+    @IBOutlet weak var previewButton: yellowButtonView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
